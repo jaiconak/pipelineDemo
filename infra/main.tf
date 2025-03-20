291a603f9a0af57143e6273157d009319e957843
Jaico Seth Nakpil <jaiconak@gmail.com>
2025-03-19 22:09:58 -0400

    code change

--- infra/main.tf
+++ infra/main.tf
@@ -1,61 +1,6 @@
-provider "aws" {
-  region = var.region
-}
-
-resource "aws_vpc" "testingInstance" {
-  cidr_block       = "10.20.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "VPC-${var.env}"
-  }
-}
-
-resource "aws_internet_gateway" "IG" {
-  vpc_id = aws_vpc.testingInstance.id
-
-  tags = {
-    Name = "IG-${var.env}"
-  }
-}
-
-resource "aws_subnet" "vpc1" {
-  vpc_id     = aws_vpc.testingInstance.id
-  cidr_block = "10.20.1.0/24"  # Subnet must be a subset of the VPC CIDR
-
-  tags = {
-    Name = "Subnet-${var.env}"
-  }
-}
-
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.testingInstance.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.IG.id
-  }
-
-  tags = {
-    Name = "RouteTable-${var.env}"
-  }
-}
-
-resource "aws_route_table_association" "rta" {
-  subnet_id      = aws_subnet.vpc1.id
-  route_table_id = aws_route_table.rt.id
-}
-
-resource "aws_instance" "serv1" {
-  ami           = "ami-04aa00acb1165b32a"
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.vpc1.id
-
-  tags = {
-    Name = var.env
-  }
-}
-
+########################################
+# Variables
+########################################
 variable "env" {
   description = "Environment name (e.g., Dev, QA, UAT)"
 }
@@ -65,5 +10,81 @@ variable "region" {
 }
 
 variable "instance_type" {
-  description = "EC2 Instance Type (e.g., t2.large, t3.medium)"
+  description = "EC2 Instance Type (e.g., t2.micro, t2.large)"
+}
+
+########################################
+# AWS Provider
+########################################
+provider "aws" {
+  region = var.region
+}
+
+########################################
+# VPC
+########################################
+resource "aws_vpc" "main" {
+  cidr_block = "10.20.0.0/16"
+
+  tags = {
+    Name = "VPC-${var.env}"
+  }
+}
+
+########################################
+# Internet Gateway
+########################################
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "IGW-${var.env}"
+  }
+}
+
+########################################
+# Subnet
+########################################
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.20.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Subnet-${var.env}"
+  }
+}
+
+########################################
+# Route Table & Association
+########################################
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "RouteTable-${var.env}"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+########################################
+# EC2 Instance
+########################################
+resource "aws_instance" "main" {
+  ami           = "ami-04aa00acb1165b32a"
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.main.id
+
+  tags = {
+    Name = "Instance-${var.env}"
+  }
 }
